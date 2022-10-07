7a0a8557f8d96623028d7a55611c313984d2b873
Rachit <bhojanirachit@gmail.com>
2022-10-07 15:21:22 +0530

    add Q2_5_1 pattern code

---
+++ Q2_5_1.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class Q2_5_1
+{
+public static void main(String args[])
+{
+int i,j,n;
+Scanner sc =new Scanner(System.in);
+System.out.println("Enter a number");
+n=sc.nextInt();
+for(i=1;i<=n;i++)
+{
+if(i%2==0)
+{
+for(j=i;j>=1;j--)
+{
+System.out.print(j+" ");
+}
+}
+else
+{
+for(j=1;j<=i;j++)
+{
+System.out.print(j+" ");
+}
+
+}
+
+
+System.out.println();
+}
+
+
+}
+}
