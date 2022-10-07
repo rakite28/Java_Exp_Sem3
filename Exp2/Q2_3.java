641aae229311e81b83dcba5dff9b78cf7d4fd28b
Rachit <bhojanirachit@gmail.com>
2022-10-07 15:50:13 +0530

    add primeNumber counter code

---
+++ Exp2/Q2_3.java
@@ -0,0 +1,30 @@
+package Exp2;
+import java.util.*;
+public class Q2_3 
+{
+    public static void main(String args[])
+    {
+        int i,a,b,j,c=0;
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter start no.");
+            a=sc.nextInt();
+            System.out.println("Enter end no.");
+            b=sc.nextInt();
+        }
+        for(i=a;i<=b;i++)
+        {
+            //if(i%2==0) for odd numbers
+            c=0;
+            for(j=2;j<i/2+1;j++)
+            {
+                if(i%j==0)
+                c++;
+            }
+            if(c==0)
+            System.out.println(i);
+            else
+            continue;
+        }
+
+    }
+}
