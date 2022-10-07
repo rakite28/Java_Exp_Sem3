9b7846e23390439bb749ac7468b564dafc44759e
Rachit <bhojanirachit@gmail.com>
2022-10-07 15:29:35 +0530

    correct quadraticRoots code

--- Exp2/quadraticRoots.java
+++ Exp2/quadraticRoots.java
@@ -1,7 +1,7 @@
 package Exp2;
 import java.util.*;
 public class quadraticRoots {
-    public static void main(String[] args)
+    public static void main(String args[])
     {
             Scanner sc = new Scanner(System.in);
             System.out.print("Enter coefficients: ");
