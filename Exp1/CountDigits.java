dfd946fd8c80502806d36ea04e2e5455c04cd58a
Rachit <bhojanirachit@gmail.com>
2022-10-07 15:33:20 +0530

    add countDigits code

---
+++ Exp1/CountDigits.java
@@ -0,0 +1,18 @@
+package Exp1;
+import java.util.*;
+
+public class CountDigits {
+    public static void main(String args[])
+    {
+        int i=0,n;
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter a no.");
+        n=sc.nextInt();
+        while(n!=0)
+        {
+            i++;
+            n=n/10;
+        }
+        System.out.println("The number of digits are"+i);
+    }
+}
